6d6e147478f8152d5f8a53237d5554394f85c2cf
Renaud17 <45036260+Renaud17@users.noreply.github.com>
2021-06-30 22:11:43 +0200

    Update and rename data.json to data.py

---
+++ data.py
@@ -0,0 +1,12 @@
+data = [["Salut",0],["Bonjour",0],["Bonsoire",0],["Hello",0],["coucou",0],["ça va?",0],["slt",0],["cc",0],
+["Quel est ton nom?.",1],["qu'est-ce que tu es?",1],["qui es-tu?",1],["ton nom s'il te plaît?",1],
+["Bye",2],["A plus tard",2],["Au revoir",2],["Ravi d'avoir discuté avec toi, bye",2],["A la prochaine fois",2],
+["Merci",3],["C'est utile",3],["Génial, merci",3],["Merci de m'avoir aidé",3],
+["Merci",3],["C'est utile",3],["Génial, merci",3],["Merci de m'avoir aidé",3],
+["Comment peux-tu m'aider?",4],["Que peux-tu faire?",4],["Quelle aide fournis-tu?",4],["En quoi es-tu utile?",4],["Quel soutien offre-tu ?",4],["Que sais-tu ?",4],["De quoi s'agit-il ?",4],
+["quel sont les dangers et risques dans l'entreprise ou la direction",5],["comment prévenir les dangers et risques dans l'entreprise ou la direction",5],
+["oui",6],["d'accord",6],["yes",6],
+["Travail  sur écran ",7],["travail  sur écran ",7],
+["Service de gardienage ",8],["service de gardienage ",8],
+["Magasin ",9],["magasin ",9],
+["Entretien des locaux ",10],["entretien des locaux ",10]]
