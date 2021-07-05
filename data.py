8c6b81cb85a9365cef6fd21b989d7068e164db13
Renaud17 <45036260+Renaud17@users.noreply.github.com>
2021-07-05 17:24:44 +0200

    Update data.py

--- data.py
+++ data.py
@@ -126,5 +126,3 @@ data = [["Salut",0],["Bonjour",0],["Bonsoire",0],["Hello",0],["coucou",0],["ça 
 ["règle travaux hauteur",123],["règle travail hauteur",123],["règles travaux hauteur",123],["règles travail hauteur",123],["règles de sécurité travail hauteur",123],
 ["travail avec des engins et des véhicules au chantier",124], ["travail engins  véhicules  chantier",124],["Travail engin  véhicule  chantier",124],
 ["Grue à tour",125],["grue à tour",125],["utilisation grue à tour",125],["Utilisation grue à tour",125],["règle sécurité grue à tour",125]]
-
-]
