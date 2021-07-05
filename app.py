944c166e77aa6c7a822f98cefab0a3b402950db3
Renaud17 <45036260+Renaud17@users.noreply.github.com>
2021-07-05 22:53:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -169,7 +169,8 @@ while True:
                 #print(message)
             except:
                 message = None
-            from_ = item["message"]["from"]["id"]
-            #print(from_)
-            reply = make_reply(message)
+            try:
+                from_ = item["message"]["from"]["id"]
+                #print(from_)
+                reply = make_reply(message)
             tbot.send_message(reply,from_)
