e391b211e37cf63acb540cfa054349bc01409664
Renaud17 <45036260+Renaud17@users.noreply.github.com>
2021-07-07 01:06:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -171,10 +171,9 @@ def make_reply(msg):     # user input will go here
 while True:
     print("...")
     # Request updates after the last update_id
-    for update in tbot.get_updates(offset=update_id, timeout=10):
-        update_id = update.update_id + 1
+    updates = tbot.get_updates(offset=update_id, timeout=10)
     #updates = tbot.get_updates(offset=update_id)
-    updates = update['result']
+    updates = updates['result']
     #print(updates)
     if updates:
         for item in updates:
