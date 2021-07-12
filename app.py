8a023459be03ba868655e4633fba9bc61d1817e6
Renaud17 <45036260+Renaud17@users.noreply.github.com>
2021-07-12 14:05:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ class telegram_bot():
         self.url = f"https://api.telegram.org/bot{self.token}"
 
     def get_updates(self,offset=None):
-        url = self.url+"/getUpdates?timeout=999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999"
+        url = self.url+"/getUpdates?timeout=100"
         if offset:
             url = url+f"&offset={offset+1}"
         url_info = requests.get(url)
