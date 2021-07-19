63ce84dff02f418a4f012168417b89484cbab3e0
Renaud17 <45036260+Renaud17@users.noreply.github.com>
2021-07-19 15:35:43 +0200

    Update data.py

--- data.py
+++ data.py
@@ -165,4 +165,8 @@ data = [["Salut",0],["Bonjour",0],["Bonsoire",0],["Hello",0],["coucou",0],["ça 
 ["chariot élévateur",162],["Chariot élévateur",162],["chariots élévateurs",162],["Chariots élévateurs",162],
 ["en cas d'incendie",163],["en cas d'incendies",163],["cas incendie",163],["cas incendies",163],
 ["en cas d'urgence",164],["en cas d'urgences",164],["cas urgence",164],["cas urgences",164],
-["en cas d'évacuation",165],["en cas d'évacuations",165],["cas évacuation",165],["cas évacuations",165]]
+["en cas d'évacuation",165],["en cas d'évacuations",165],["cas évacuation",165],["cas évacuations",165],
+["Panneaux lutte contre l’incendie",166],["panneaux lutte contre incendie",166],["panneau lutte contre incendie",166],
+["Panneaux  sauvetage  secours",167],["panneau  sauvetage  secour",167],
+["Panneaux d’avertissements",167],["panneaux d’avertissement",167],["Panneau avertissements",167],["panneau avertissement",167],
+["Panneaux  sauvetage secours",168],["panneau  sauvetage secour",168]]
