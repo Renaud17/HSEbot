61ea170a1ee1329c5b7d9eb73beba0e1b342b3dd
Renaud17 <45036260+Renaud17@users.noreply.github.com>
2021-07-19 16:30:48 +0200

    Update data.py

--- data.py
+++ data.py
@@ -77,7 +77,7 @@ data = [["Salut",0],["Bonjour",0],["Bonsoire",0],["Hello",0],["coucou",0],["ça 
 ["Atteinte occulaires",76],["Atteinte occulaire",76],["atteinte occulaire",76],["Refraction de la lumière par les tolles",76],
 ["Travail avec du courant électrique",77],
 ["Plomberie",78],["plomberie",78],
-["Flamme de chalumeau, soudage à l'arc",79],["Brûlure, incendie, explosion",79],["incendie",79],["d'incendie",79],["l'incendie",79],["Produits inflammables ",79],["produit inflammable",79],
+["Flamme de chalumeau, soudage à l'arc",79],["Brûlure, incendie, explosion",79],["incendie",79],["d'incendie",79],["l'incendie",79],["incendies",79],["Produits inflammables ",79],["produit inflammable",79],
 ["Travail avec l'électricité",80],["Electrocution, Electricité",80],["électrocution, électricité",80],["électrocutions, électricités",80],["électrocution,électricité",80],["Electrocutions,Electricités",80],
 ["Espace encombré",81],["Chute de plain-pied",81],["espace encombré",81],["chute de plain-pied",81],["espaces encombrés",81],
 ["Dégagement des poussières",82],["Inhalation des poussières",82],["dégagement de poussière",82],["inhalation de poussière",82],
