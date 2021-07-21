0df117210e0634967c16c508fa6e2db1c53af686
Renaud17 <45036260+Renaud17@users.noreply.github.com>
2021-07-22 00:54:10 +0200

    Update appV2.py

--- appV2.py
+++ appV2.py
@@ -130,7 +130,7 @@ def bot_initialize(user_msg):
                 update_name = name_list[name_list['CAT']==resp]
                 A=list(update_name['URL'])
                 listToStr = '\n'.join(map(str, A))
-                return listToStr
+                return "PICTOGRAMME"+ str(listToStr)
             
             else:
                 resp = "Désolé je ne comprend pas mon vocabulaire est en amélioration.Envoie ta question à mon créateur @Renaud17" #random.choice(responses[4]['response'])
