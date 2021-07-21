108a9725ab47543d3f90ae6859143c819e56e98e
Renaud17 <45036260+Renaud17@users.noreply.github.com>
2021-07-21 10:42:48 +0200

    Update appV2.py

--- appV2.py
+++ appV2.py
@@ -138,8 +138,8 @@ def bot_img(user_msg):
         user_intent = intent(user_response)
         if (user_response == 'affiche'):
             user_response=user_response.lower()
-                file_path =  response(user_response)
-                return file_path     
+            file_path =  response(user_response)
+            return file_path     
         
 def send_photo(chat_id, file_opened):
     method = "sendPhoto"
