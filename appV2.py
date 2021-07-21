fe138b196dc04c8a8e9c00e8b08529d5dd6d5c8f
Renaud17 <45036260+Renaud17@users.noreply.github.com>
2021-07-21 20:57:44 +0200

    Update appV2.py

--- appV2.py
+++ appV2.py
@@ -128,8 +128,8 @@ def bot_initialize(user_msg):
                 user_response=user_response.lower()
                 resp =  response(user_response)
                 update_name = name_list[name_list['CAT']==resp]
-                for i in range(0,len(update_name))::
-                    return "https://i.imgur.com/" + update_name.iloc[i,0]
+                for i in range(0,len(update_name)):
+			return "https://i.imgur.com/" + update_name.iloc[i,0]
             
             else:
                 resp = "Désolé je ne comprend pas mon vocabulaire est en amélioration.Envoie ta question à mon créateur @Renaud17" #random.choice(responses[4]['response'])
