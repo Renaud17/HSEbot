f1d8d54242be4113ab9c9c72c5f97af4bcb1818a
Renaud17 <45036260+Renaud17@users.noreply.github.com>
2021-07-21 18:10:35 +0200

    Update appV2.py

--- appV2.py
+++ appV2.py
@@ -123,13 +123,15 @@ def bot_initialize(user_msg):
                 user_response=user_response.lower()
                 resp =  response(user_response)
                 return resp #+ "\n\n🎁CADEAU🎁\nJe t'offre ce document HSE qui te servira pour tes TBM et répondre à certaines questions dont ma réponse te semble incorrecte je suis une intelligence artificielle et je peux faire des erreurs comme l'humain.😊:\n https://drive.google.com/file/d/10nDPjBZZX82XCQUZIlUCujc0PpYDlWhb/view?usp=sharing"
-	    elif (user_intent == "affiche"):
-		user_response=user_response.lower()
-		resp =  response(user_response)
-		update_name = name_list[name_list['CAT']==resp]
-		for i in range(0,len(update_name)):
-			url = "https://i.imgur.com/" + update_name.iloc[i,0]
-		return url
+            
+            elif (user_intent == "affiche"):
+                user_response=user_response.lower()
+                resp =  response(user_response)
+                update_name = name_list[name_list['CAT']==resp]
+                for i in range(0,len(update_name)):
+                    url = "https://i.imgur.com/" + update_name.iloc[i,0]
+                return url
+            
             else:
                 resp = "Désolé je ne comprend pas mon vocabulaire est en amélioration.Envoie ta question à mon créateur @Renaud17" #random.choice(responses[4]['response'])
                 return resp
